2ec404b3cbe53f954694b275f4157a5ff8a8eaf8
Anastasiia bc2025-5 <anastasiia.fradkova@gmail.com>
2026-04-10 13:09:54 +0300

    Added GET, PUT and DELETE methods processing.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { program } = require('commander');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const fsPromises = require('fs').promises;
 
 program
   .requiredOption('-h, --host <type>')
@@ -17,9 +18,47 @@ if (!fs.existsSync(cacheDir)) {
   fs.mkdirSync(cacheDir, { recursive: true });
 }
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    response.end('Hello world!');
+const server = http.createServer(async (request, response) => {
+  const code = request.url.slice(1);
+  const filePath = path.join(cacheDir, `${code}.jpg`);
+
+  switch (request.method) {
+    case "GET":
+      try {
+        const data = await fsPromises.readFile(filePath);
+        response.writeHead(200, "Content-Type: image/jpeg");
+        response.end(data);
+      } catch (error) {
+        response.writeHead(404);
+        response.end("File not found.");
+      }
+      break;
+    case "PUT":
+      const chunks = [];
+      request.on("data", (chunk) => {
+        chunks.push(chunk);
+      });
+      request.on("end", async () => {
+        const body = Buffer.concat(chunks);
+        await fsPromises.writeFile(filePath, body);
+        response.writeHead(201);
+        response.end("File was successfully added.");
+      });
+      break;
+    case "DELETE":
+      try {
+        await fsPromises.unlink(filePath);
+        response.writeHead(200);
+        response.end("File was successfully deleted.");
+      } catch (error) {
+        response.writeHead(404);
+        response.end("File not found.");
+      }
+      break;
+    default:
+      response.writeHead(405);
+      response.end("Method not allowed.");
+  }
 });
 
 server.on('error', (e) => {
